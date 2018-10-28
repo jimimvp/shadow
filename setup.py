fb3f84f324ffabbf3be267ca76f15d9bb2c782f3
Marin Vlastelica <marin.vlastelicap@gmail.com>
2018-10-28 12:16:24 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,6 @@ from setuptools import setup
 from setuptools import setup
 
 setup(
-    name='hello',
-    version='0.1',
-    py_modules=['hello'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
-        [console_scripts]
-        hello=hello:say_hello
-    ''',
-)
-
-setup(
     name='shadow',
     version='0.1',
     keywords='Easy commandline encryption',
@@ -24,11 +11,10 @@ setup(
     install_requires=[
         'tar-progress',
         'click',
-        'tqdm',
-        'python-gnugpg'
-    ],
+        'tqdm'
+        ],
     entry_points='''
         [console_scripts]
-        shadow=cli:main
+        shadow=shadow.cli:main
     '''
 )
